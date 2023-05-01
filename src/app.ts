9f2adf60ebcf878e5c2da7f80184be115a191adc
Larbz <luisarroyo0330@gmail.com>
2023-05-01 17:22:46 -0500

    Finish authentication

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
-import express, { Application } from "express";
+import "dotenv/config";
+import express, { Application, json } from "express";
+import morgan from "morgan";
+import authRoutes from "./routes/auth";
 
 const app: Application = express();
-
 //settings
-app.set("port", 3000);
+app.set("port", process.env.PORT);
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+//routes
+app.use("/api/auth", authRoutes);
 
 export default app;
